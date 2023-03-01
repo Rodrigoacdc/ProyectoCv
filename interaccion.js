1f162083ab34f8717628dd445a886b2a15794ba6
rodrigoacdc <rodrigodsalas94@gmail.com>
2023-03-01 13:56:58 -0300

    first commit

---
+++ interaccion.js
@@ -0,0 +1,8 @@
+document.getElementById("boton").onclick= function() {
+    
+    document.getElementById("demo").innerHTML= "Barrio Gran Capitán M:D, C:22; La Colonia, Junin, Mendoza"
+    }
+    document.getElementById("boton1").onclick= function() {
+        
+    document.getElementById("demo1").innerHTML= "Celular: 2634746992 Fijo: 2634426913"  
+    }
